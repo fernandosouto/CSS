dd161031d2b761f42b31721dfa9c5434b9ac0a6f
Fernando Souto <fernandosouto.servicos@gmail.com>
2021-09-16 12:54:25 -0300

    Atualização de estudos 16/09

---
+++ pages/js/flexbox.js
@@ -0,0 +1,9 @@
+console.log("Javascript Ativo");
+
+window.onload = function () {
+    const flexb = document.getElementsByClassName("flexbox");
+    
+    flexb[0].style.backgroundColor = "#000";
+    flexb[1].style.backgroundColor = "#222";
+    flexb[2].style.backgroundColor = "#444";
+}
