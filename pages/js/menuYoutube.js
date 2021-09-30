1792025170d67a2bef47007d537d53b1f81ad48f
Fernando Souto <fernandosouto.servicos@gmail.com>
2021-09-30 09:32:37 -0300

    Atualização do Estudo Youtube

--- pages/js/menuYoutube.js
+++ pages/js/menuYoutube.js
@@ -3,3 +3,12 @@ function apaga(){
     let texto = document.getElementsByClassName("textao")[0];
     texto.value = '';
 }
+function verticalDisplay(){
+    console.log("Chamou Função");
+    var ver = document.querySelector(".vertical");
+     if(ver.style.display === "inline-flex"){
+         ver.style.display = "none"
+     }else{
+         ver.style.display = "inline-flex"
+     }
+}
